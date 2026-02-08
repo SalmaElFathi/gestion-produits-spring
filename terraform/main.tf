4d11aea420fe3c2309166559d688ded275637b8c
Salma El Fathi <fathisalma374@gmail.com>
2026-02-08 11:47:56 +0100

    Fix Terraform syntax: close vagrant_up resource block

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,42 @@
-terraform {
-  required_version = ">= 1.3.0"
+terraform { 
+  required_version = ">= 1.3.0" 
 
-  required_providers {
-    null = {
-      source  = "hashicorp/null"
-      version = "~> 3.2"
-    }
-  }
-}
+  required_providers { 
+    null = { 
+      source  = "hashicorp/null" 
+      version = "~> 3.2" 
+    } 
+  } 
+} 
 
-########################################
-# 1️⃣ Créer les VMs via Vagrant
-########################################
+######################################## 
+# 1️⃣ Créer les VMs via Vagrant 
+######################################## 
 
-resource "null_resource" "vagrant_up" {
-  triggers = {
-    vagrantfile_hash = filesha256("${path.module}/Vagrantfile")
-  }
+resource "null_resource" "vagrant_up" { 
+  triggers = { 
+    vagrantfile_hash = filesha256("${path.module}/Vagrantfile") 
+  } 
 
-provisioner "local-exec" {
-  command     = "vagrant up"
-  working_dir = path.module
-}
+  provisioner "local-exec" { 
+    command     = "vagrant up" 
+    working_dir = path.module 
+  } 
+}  
 
-########################################
-# 2️⃣ Provisionnement Ansible
-########################################
+######################################## 
+# 2️⃣ Provisionnement Ansible 
+######################################## 
 
-resource "null_resource" "ansible" {
-  depends_on = [null_resource.vagrant_up]
+resource "null_resource" "ansible" { 
+  depends_on = [null_resource.vagrant_up] 
 
-  provisioner "local-exec" {
-    command = <<EOT
-      ansible-playbook -i ../ansible/inventory ../ansible/docker.yml
-      ansible-playbook -i ../ansible/inventory ../ansible/k8s-common.yml
-      ansible-playbook -i ../ansible/inventory ../ansible/k8s-master.yml
-      ansible-playbook -i ../ansible/inventory ../ansible/k8s-workers.yml
-    EOT
-  }
+  provisioner "local-exec" { 
+    command = <<EOT 
+      ansible-playbook -i ../ansible/inventory ../ansible/docker.yml 
+      ansible-playbook -i ../ansible/inventory ../ansible/k8s-common.yml 
+      ansible-playbook -i ../ansible/inventory ../ansible/k8s-master.yml 
+      ansible-playbook -i ../ansible/inventory ../ansible/k8s-workers.yml 
+    EOT 
+  } 
 }
